94ffc57c97b55524b51f1f44a96b13a07add3561
AsmaaElManshawy <sayedasmaa2001@gmail.com>
2026-04-14 16:56:41 +0200

    Add project files.

---
+++ Helpers/StringCaseInsensitiveComparer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace LINQAssignment02.Helpers
+{
+    public class StringCaseInsensitiveComparer : IComparer<string>
+    {
+        public int Compare(string? x, string? y)
+        {
+            return string.Compare(x, y, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace LINQAssignment02.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal Total { get; set; }
+        public override string ToString() => $"{OrderID}: {OrderDate:d} for {Total:C2}";
+    }
+
+}
